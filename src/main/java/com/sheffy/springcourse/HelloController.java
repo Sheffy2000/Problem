45da9355796dad00477b5a43d661d8e49c1095f8
Sheffy <ishevchenko.id2000@yandex.ru>
2025-02-27 17:02:49 +0300

    web.xml

---
+++ src/main/java/com/sheffy/springcourse/HelloController.java
@@ -0,0 +1,12 @@
+package com.sheffy.springcourse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+    @GetMapping("/hello-world")
+    public String sayHelo() {
+        return "hello_world";
+    }
+}
