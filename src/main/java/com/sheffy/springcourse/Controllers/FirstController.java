0966feb71c565fafb1c6e3dcbb265e8f90f3684a
Sheffy <ishevchenko.id2000@yandex.ru>
2025-02-28 15:30:16 +0300

    views

---
+++ src/main/java/com/sheffy/springcourse/Controllers/FirstController.java
@@ -0,0 +1,16 @@
+package com.sheffy.springcourse.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FirstController {
+    @GetMapping("/hello")
+    public String helloPage() {
+        return "first/hello";
+    }
+    @GetMapping("/goodbye")
+    public String goodbyePage() {
+        return "first/goodbye";
+    }
+}
